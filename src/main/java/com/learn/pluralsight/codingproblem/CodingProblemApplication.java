76c1c95a50f5965f185639b2493540eeb64529ab
Roshan Deniyage <crdeniyage@gmail.com>
2018-07-20 15:32:33 +1200

    first commit

---
+++ src/main/java/com/learn/pluralsight/codingproblem/CodingProblemApplication.java
@@ -0,0 +1,12 @@
+package com.learn.pluralsight.codingproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingProblemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/learn/pluralsight/codingproblem/CodingProblemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.pluralsight.codingproblem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodingProblemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
